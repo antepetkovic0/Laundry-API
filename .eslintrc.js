7c1feb392f25ad92b2cac811ced8f4296d7382f5
bigPoppa-x <ante.petkovic01@gmail.com>
2021-04-11 12:58:56 +0200

    init, lint setup

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: ["airbnb-base", "prettier", "plugin:node/recommended"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+  },
+};
